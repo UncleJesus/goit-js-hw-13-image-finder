ccbb3ac9f01140c40de4981ce51dcba4b849c962
Омельянов Дима <domelyanov@hotmail.com>
2020-11-05 15:43:36 +0200

    с

--- src/index.js
+++ src/index.js
@@ -3,8 +3,3 @@ import { input } from './js/searchImage.js';
 import { searchImage } from './js/searchImage.js';
 
 input.addEventListener('change', searchImage);
-
-// window.scrollTo({
-//   top: 1000,
-//   behavior: 'smooth',
-// });
